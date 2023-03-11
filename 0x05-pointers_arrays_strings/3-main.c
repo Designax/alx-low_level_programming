e53314a653854152db27662f7a768f469e0cd447
Designax <miftoneyu@gmail.com>
2023-03-11 07:03:56 -0800

    3-puts fix

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - check the code
  *
@@ -10,6 +9,6 @@ int main(void)
 char *str;
 
 str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-puts(str);
+_puts(str);
 return (0);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <string.h>
 /**
  * _puts - this is a fuction that puts
  * @str: a param to _puts fuction
  */
 void _puts(char *str)
 {
-for (; *str != '\0'; str++)
-{
-_putchar(*str);
-}
-_putchar('\n');
+	for (; *str != '\0'; str++)
+	{
+		_putchar(str);
+	}
+	_putchar('\n');
 }
