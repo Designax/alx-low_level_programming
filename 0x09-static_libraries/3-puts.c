8ce8e7fae019708478af5bc30583fe34f5f44378
Designax <miftoneyu@gmail.com>
2023-03-21 06:31:42 -0700

    compile

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
@@ -8,7 +8,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		putchar(*str++);
+		_putchar(*str++);
 	}
-		putchar('\n');
+		_putchar('\n');
 }
